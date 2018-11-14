4869d97600de8521c48982a35fc90c2ee7b6806e
y.romanchuk <anybis2007@gmail.com>
2018-11-14 17:32:38 +0300

    test2

--- src/ClassHello.java
+++ src/ClassHello.java
@@ -2,5 +2,6 @@ public class ClassHello {
     public static  void main(String[] args) {
         System.out.println("фыав");
         System.out.println("ffff");
-    }
+        System.out.println("dddd");
+}
 }
