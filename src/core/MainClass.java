87f1fb65a0e3bc96eadd93b20a43bc217dd5c6f7
y.romanchuk <anybis2007@gmail.com>
2018-12-12 13:34:22 +0200

    DistanceBetweenPoints lesson 4

--- src/core/MainClass.java
+++ src/core/MainClass.java
@@ -9,8 +9,12 @@ public class MainClass {
         //Lesson4FiguresSquare area = new Lesson4FiguresSquare();
        // area.Lesson4FiguresSquare();
 
-        Termometr grad = new Termometr();
-        grad.Termometr();
+        /*Termometr grad = new Termometr();
+        grad.Termometr();*/
+
+        DistanceBetweenPoints distance = new DistanceBetweenPoints();
+        distance.distanceBetweenPoints();
+
     }
 
     public void runLessons(){
