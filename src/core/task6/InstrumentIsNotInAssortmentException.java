700094d11826d70fb53d42215403d5fcbb5899af
yromanchuk <anybis2007@gmail.com>
2018-12-26 23:36:23 +0200

    task 6

---
+++ src/core/task6/InstrumentIsNotInAssortmentException.java
@@ -0,0 +1,8 @@
+package core.task6;
+
+public class InstrumentIsNotInAssortmentExceprion extends Exception {
+
+    public InstrumentIsNotInAssortmentExceprion(String s) {
+        super(s);
+    }
+}
